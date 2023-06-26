cc143e7268219fa12727d10e786c145df40eab59
yunse0708 <user.email>
2023-06-26 20:21:45 +0900

    1090

---
+++ 단순 반복문/1090.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	long long int a,r,n;  
+	
+	scanf("%lld %lld %lld",&a, &r, &n); 
+    
+	for(int i=1; i<n; i++){ 
+		a = a * r;	
+	}
+	printf("%lld", a);
+	
+	return 0;
+} 
