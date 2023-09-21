53cea476cf10ecf66071988721500a406332b06a
yunse0708 <Yunse070708@gmail.com>
2023-09-22 01:02:25 +0900

    6066

--- python 기초 100제.py/6066.py
+++ python 기초 100제.py/6066.py
@@ -1,2 +1,12 @@
 
+a, b, c = map(int, input().split())
+if ( a % 2 != 0 ) :
+    print("odd")
+else : print("even")
+if ( b % 2 != 0 ) :
+    print("odd")
+else : print("even")
+if ( c % 2 != 0 ) :
+    print("odd")
+else : print("even")
 
